513bb721e8058f6e878773f984f9dc905832991c
Luís Fernando Moraes <luisfernando.pereira.moraes@gmail.com>
2019-09-28 19:35:06 -0300

    Add project files.

---
+++ Inventti.CodeChallenge.PrimeString.Domain/Models/PrimeString.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using FluentValidator;
+
+namespace Inventti.CodeChallenge.PrimeString.Domain.Models
+{
+    /// <summary>
+    /// Objeto que identifica se as strings são primas.
+    /// </summary>
+    public class PrimeString : Notifiable
+    {
+        private readonly string _firstString;
+        private readonly string _secondString;
+
+        /// <summary>
+        /// Inicializa um objeto PrimeString.
+        /// </summary>
+        /// <param name="firstString">Primeira string usada na comparação.</param>
+        /// <param name="secondString">Segunda string usada na comparação.</param>
+        public PrimeString(string firstString, string secondString)
+        {
+            if (string.IsNullOrEmpty(firstString))
+                AddNotification(nameof(firstString), "O valor não pode ser nulo ou vazio.");
+            _firstString = firstString;
+            if (string.IsNullOrEmpty(secondString))
+                AddNotification(nameof(secondString), "O valor não pode ser nulo ou vazio.");
+            _secondString = secondString;
+        }
+
+        /// <summary>
+        /// Verifica se as strings são primas.
+        /// A Definição de string prima é a seguinte:
+        /// "Uma String é considerada prima se ambas tem o mesmo tamanho, e se todos os caracteres em posições impares na primeira String estão em posições impares na segunda String,
+        /// e se todos os caracteres em posições pares na primeira String estão em posições pares na segunda String."
+        /// </summary>
+        /// <returns></returns>
+        public bool ArePrimes()
+        {
+            if (_firstString.Length != _secondString.Length || string.IsNullOrEmpty(_firstString))
+                return false;
+
+            var resultForEven = CheckEvenPositions(_firstString, _secondString);
+            var resultForOdd = CheckOddPositions(_firstString, _secondString);
+
+            return resultForEven && resultForOdd;
+        }
+
+        /// <summary>
+        /// Verifica se as strings são primas analisando os indices pares.
+        /// </summary>
+        /// <param name="firstString">Primeira string usada na comparação.</param>
+        /// <param name="secondString">Segunda string usada na comparação.</param>
+        /// <returns></returns>
+        private bool CheckEvenPositions(string firstString, string secondString)
+        {
+            var evenElementsFromFirstString =
+                _firstString.Select((value, index) => (value, index)).Where(item => item.index % 2 == 0).ToList();
+
+            var evenElementsFromSecondString =
+                _secondString.Select((value, index) => (value, index)).Where(item => item.index % 2 == 0).ToList();
+
+
+            var result = evenElementsFromFirstString.TrueForAll(valueTuple=> evenElementsFromSecondString.Exists(x => x.value == valueTuple.value));
+
+            return result;
+        }
+
+        /// <summary>
+        /// Verifica se as strings são primas analisando os indices impares.
+        /// </summary>
+        /// <param name="firstString">Primeira string usada na comparação.</param>
+        /// <param name="secondString">Segunda string usada na comparação.</param>
+        /// <returns></returns>
+        private bool CheckOddPositions(string firstString, string secondString)
+        {
+            var oddElementsFromFirstString =
+                _firstString.Select((value, index) => (value, index)).Where(item => item.index % 2 != 0).ToList();
+
+            var oddElementsFromSecondString =
+                _secondString.Select((value, index) => (value, index)).Where(item => item.index % 2 != 0).ToList();
+
+            var result = oddElementsFromFirstString.TrueForAll(valueTuple=> oddElementsFromSecondString.Exists(x => x.value == valueTuple.value));
+
+            return result;
+        }
+    }
+}
---
+++ Inventti.CodeChallenge.PrimeString.Test/Unit/PrimeStringTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Inventti.CodeChallenge.PrimeString.Test.Unit
+{
+    public class PrimeStringTest
+    {
+        [Fact(DisplayName = "Validação de parâmetros incorretos.")]
+        public void Should_Be_Invalid_Values_For_Prime_String()
+        {
+            // Arr 
+            var primeString = new Domain.Models.PrimeString("", "");
+
+            // Act
+            var result = primeString.ArePrimes();
+            
+            // Ass
+            primeString.Valid.Should().BeFalse();
+            result.Should().BeFalse();
+        }
+
+        [Fact(DisplayName = "Objeto em estado invalido com duas notificações de domínio")]
+        public void Should_Be_Invalid_Values_For_Prime_String_With_Two_Notification()
+        {
+            // Arr 
+            var primeString = new Domain.Models.PrimeString("", "");
+
+            // Act
+            var result = primeString.ArePrimes();
+            
+            // Ass
+            primeString.Notifications.Count.Should().Be(2);
+            primeString.Valid.Should().BeFalse();
+            result.Should().BeFalse();
+        }
+
+        [Fact(DisplayName = "Objeto em estado invalido com uma notificação de domínio")]
+        public void Should_Be_Invalid_Values_For_Prime_String_With_One_Notification()
+        {
+            // Arr 
+            var primeString = new Domain.Models.PrimeString("test", "");
+
+            // Act
+            var result = primeString.ArePrimes();
+            
+            // Ass
+            primeString.Notifications.Count.Should().Be(1);
+            primeString.Valid.Should().BeFalse();
+            result.Should().BeFalse();
+        }
+
+        [Theory(DisplayName = "Strings devem ser primas")]
+        [InlineData("abcd", "cdab")]
+        [InlineData("sacada", "casada")]
+        public void Should_Be_Primes_Strings(string firstString, string secondString)
+        {
+            // Arr 
+            var primeString = new Domain.Models.PrimeString(firstString, secondString);
+
+            // Act
+            var result = primeString.ArePrimes();
+            
+            // Ass
+            primeString.Notifications.Count.Should().Be(0);
+            primeString.Valid.Should().BeTrue();
+            result.Should().BeTrue();
+        }
+
+        [Theory(DisplayName = "Strings não devem ser primas")]
+        [InlineData("oi", "ola")]
+        [InlineData("abcd", "dcba")]
+        [InlineData("elvis", "lives")]
+        public void Should_Be_Not_Primes_Strings(string firstString, string secondString)
+        {
+            // Arr 
+            var primeString = new Domain.Models.PrimeString(firstString, secondString);
+
+            // Act
+            var result = primeString.ArePrimes();
+            
+            // Ass
+            primeString.Notifications.Count.Should().Be(0);
+            primeString.Valid.Should().BeTrue();
+            result.Should().BeFalse();
+        }
+    }
+}
